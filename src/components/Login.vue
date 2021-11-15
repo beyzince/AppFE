46246ae7b3177838bd38b703a4386834c7ef35b0
Servet Ozboke <sozboke@etu.edu.tr>
2021-11-15 16:24:00 +0300

    FE Login.vue added

---
+++ src/components/Login.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <v-text-field
+                 label="Username"
+                 hide-details="auto"
+        ></v-text-field>
+        <v-text-field
+                 label="Password"
+                 type="password"
+                 hide-details="auto"
+        ></v-text-field>
+        <v-button type="button" v-on:click="login()">Login</v-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                input: {}
+            }
+        },
+        name: "Login",
+        methods: {
+            login() {
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
