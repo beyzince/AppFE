1843a0947341713eba601de826035fd239099ff2
Servet Ozboke <sozboke@etu.edu.tr>
2021-11-15 17:01:14 +0300

    New modules installed

--- src/main.js
+++ src/main.js
@@ -2,12 +2,30 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'vuetify/dist/vuetify.min.css'
 import Vuetify from 'vuetify';
+import axios from 'axios';
+import VueAxios from 'vue-axios'
+import VueRouter from 'vue-router'
+import store from "./vuex/index";
+import Login from './components/Login.vue';
 
+Vue.use(VueAxios, axios);
+Vue.use(VueRouter);
 Vue.use(Vuetify);
 
 
 Vue.config.productionTip = false;
 
+export const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {name: 'Login', path: '/login', component: Login},
+  ]
+});
+
 new Vue({
+  vuetify : new Vuetify(),
+  store: store,
+  router: router,
   render: h => h(App),
-}).$mount('#app');
+
+}).$mount('#app')
